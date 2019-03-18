5edf9f2867ba22580188bdd196f2c14aa91151b5
10XMairing <10xpumba@gmail.com>
2019-03-18 06:22:38 +0530

    updated code comments

--- node_server/index.js
+++ node_server/index.js
@@ -2,9 +2,12 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
+//server a html file
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+
+//create a socket connection
 
 io.on("connection", socket => {
   console.log("connected");
@@ -12,12 +15,14 @@ io.on("connection", socket => {
     console.log("Disconnected");
   });
 
+  //do something with message recieved, in this case emit the msg so that all clients can recieve it
   socket.on("chat message", msg => {
     console.log(msg);
     io.emit("chat message", msg);
   });
 });
 
+//run server at port 3000
 http.listen(3000, () => {
   console.log("listening on port 3000");
 });
